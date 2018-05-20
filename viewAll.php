c5fa172dae93ea848e0b17d20d600a1c485045b2
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2018-05-20 20:55:34 +0530

    viewAll is done

--- viewAll.php
+++ viewAll.php
@@ -22,7 +22,7 @@
 		if (mysqli_num_rows($is_readings_query_run)!=0){
 			if($quality=="All"){
 				while($reading_execute = mysqli_fetch_assoc($is_readings_query_run)){
-					 $dataPoints[] = array("label"=>"","y"=>"");
+					 //$dataPoints[] = array("label"=>"","y"=>"");
 					 $dataPoints1[] = array("label"=>$reading_execute["Time"],"y"=>$reading_execute["Temperature"]);
 					 $dataPoints2[] = array("label"=>$reading_execute["Time"],"y"=>$reading_execute["pH"]);
 					 $dataPoints3[] = array("label"=>$reading_execute["Time"],"y"=>$reading_execute["Turbidity"]);
@@ -33,10 +33,10 @@
 				$all = false;
 				while($reading_execute = mysqli_fetch_assoc($is_readings_query_run)){
 					 $dataPoints[] = array("label"=>$reading_execute["Time"],"y"=>$reading_execute[$quality]);
-					 $dataPoints1[] = array("label"=>"","y"=>"");
+					 /*$dataPoints1[] = array("label"=>"","y"=>"");
 					 $dataPoints2[] = array("label"=>"","y"=>"");
 					 $dataPoints3[] = array("label"=>"","y"=>"");
-					 $dataPoints4[] = array("label"=>"","y"=>"");
+					 $dataPoints4[] = array("label"=>"","y"=>"");*/
 				}
 			}
 		}
@@ -76,14 +76,15 @@
 }*/
 </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<script>
+<?php
+	if($all){
+		echo "<script>
 window.onload = function () {
-var chart = new CanvasJS.Chart("chartContainer", {
+var chart1 = new CanvasJS.Chart('chartContainer1', {
 	animationEnabled: true,
-	//theme: "light2",
+	//theme: 'light2',
 	title:{
-		text: ""
+		text: 'Temperature'
 	},
 	axisX:{
 		crosshair: {
@@ -92,7 +93,7 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		}
 	},
 	axisY:{
-		title: "",
+		title: 'Celsius ( C )',
 		crosshair: {
 			enabled: true,
 			snapToDataPoint: true
@@ -102,15 +103,15 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		enabled: false
 	},
 	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
+		type: 'area',
+		dataPoints:"; echo json_encode($dataPoints1, JSON_NUMERIC_CHECK);
+echo	"}]
 });
-var chart1 = new CanvasJS.Chart("chartContainer1", {
+var chart2 = new CanvasJS.Chart('chartContainer2', {
 	animationEnabled: true,
-	//theme: "light2",
+	//theme: 'light2',
 	title:{
-		text: "Temperature"
+		text: 'pH'
 	},
 	axisX:{
 		crosshair: {
@@ -119,7 +120,7 @@ var chart1 = new CanvasJS.Chart("chartContainer1", {
 		}
 	},
 	axisY:{
-		title: "Celsius ( C )",
+		title: 'pH',
 		crosshair: {
 			enabled: true,
 			snapToDataPoint: true
@@ -129,15 +130,15 @@ var chart1 = new CanvasJS.Chart("chartContainer1", {
 		enabled: false
 	},
 	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
-	}]
+		type: 'area',
+		dataPoints:"; echo json_encode($dataPoints2, JSON_NUMERIC_CHECK);
+echo	"}]
 });
-var chart2 = new CanvasJS.Chart("chartContainer2", {
+var chart3 = new CanvasJS.Chart('chartContainer3', {
 	animationEnabled: true,
-	//theme: "light2",
+	//theme: 'light2',
 	title:{
-		text: "pH"
+		text: 'Turbidity'
 	},
 	axisX:{
 		crosshair: {
@@ -146,7 +147,7 @@ var chart2 = new CanvasJS.Chart("chartContainer2", {
 		}
 	},
 	axisY:{
-		title: "pH",
+		title: 'Units',
 		crosshair: {
 			enabled: true,
 			snapToDataPoint: true
@@ -156,15 +157,15 @@ var chart2 = new CanvasJS.Chart("chartContainer2", {
 		enabled: false
 	},
 	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
-	}]
+		type: 'area',
+		dataPoints:"; echo json_encode($dataPoints3, JSON_NUMERIC_CHECK);
+echo	"}]
 });
-var chart3 = new CanvasJS.Chart("chartContainer3", {
+var chart4 = new CanvasJS.Chart('chartContainer4', {
 	animationEnabled: true,
-	//theme: "light2",
+	//theme: 'light2',
 	title:{
-		text: "Turbidity"
+		text: 'Conductivity'
 	},
 	axisX:{
 		crosshair: {
@@ -173,7 +174,7 @@ var chart3 = new CanvasJS.Chart("chartContainer3", {
 		}
 	},
 	axisY:{
-		title: "Units",
+		title: 'Units',
 		crosshair: {
 			enabled: true,
 			snapToDataPoint: true
@@ -183,45 +184,54 @@ var chart3 = new CanvasJS.Chart("chartContainer3", {
 		enabled: false
 	},
 	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
-	}]
+		type: 'area',
+		dataPoints:"; echo json_encode($dataPoints4, JSON_NUMERIC_CHECK);
+echo 	"}]
 });
-var chart4 = new CanvasJS.Chart("chartContainer4", {
-	animationEnabled: true,
-	//theme: "light2",
-	title:{
-		text: "Conductivity"
-	},
-	axisX:{
-		crosshair: {
-			enabled: true,
-			snapToDataPoint: true
-		}
-	},
-	axisY:{
-		title: "Units",
-		crosshair: {
-			enabled: true,
-			snapToDataPoint: true
-		}
-	},
-	toolTip:{
-		enabled: false
-	},
-	data: [{
-		type: "area",
-		dataPoints: <?php echo json_encode($dataPoints4, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
+
 chart1.render();
 chart2.render();
 chart3.render(); 
 chart4.render();
+chart.render();
 }
-</script>
-
+</script>";
+	}
+	else{
+		echo "<script>
+window.onload = function () {
+var chart = new CanvasJS.Chart('chartContainer', {
+	animationEnabled: true,
+	//theme: 'light2',
+	title:{
+		text: 'ABC'
+	},
+	axisX:{
+		crosshair: {
+			enabled: true,
+			snapToDataPoint: true
+		}
+	},
+	axisY:{
+		title: '',
+		crosshair: {
+			enabled: true,
+			snapToDataPoint: true
+		}
+	},
+	toolTip:{
+		enabled: false
+	},
+	data: [{
+		type: 'area',
+		dataPoints:"; echo json_encode($dataPoints, JSON_NUMERIC_CHECK);
+echo 	"}]
+});
+chart.render();
+}
+</script>";
+	}
+?>
 </head>
 <body>
 	<div class="container">
@@ -242,141 +252,127 @@ chart4.render();
 <?php
 if($status){
 	if($all){
-echo	"<div style='float:left;width:10%;background-color: grey;min-height:468px;'></div>
-	<div style='float:right;width:90%;'>
-		<div class='container' style='min-height: 105px;background-color: white;'>
-		<div class='row'>
-			<div class='col-md-6'>
-				<table class='table table-inverted'>
-					<h3>Temperature</h3>
-					<tbody>";
+		echo	"<div style='float:left;width:10%;background-color: grey;min-height:468px;'></div>
+				<div style='float:right;width:90%;'>
+					<div class='container' style='min-height: 105px;background-color: white;'>
+						<div class='row'>
+							<div class='col-md-6'>
+								<table class='table table-inverted'>
+								<h3>Temperature</h3>
+								<tbody>"; 
+									foreach ($dataPoints1 as $data) {
+										echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
+									}						
+		echo					"</tbody>
+								</table>
+							</div>
+							<div class='col-md-6'>
+								<div id='chartContainer1' style='min-height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class='container' style='min-height: 105px;background-color: white;'>
+					<div class='row'>
+						<div class='col-md-6'>
+							<table class='table table-inverted'>
+							<h3>pH</h3>
+							<tbody>";
+								foreach ($dataPoints2 as $data) {
+									echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
+								}
+		echo				"</tbody>
+							</table>
+						</div>
+						<div class='col-md-6'>
+							<div id='chartContainer2' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class='container' style='min-height: 105px;background-color: white;'>
+					<div class='row'>
+						<div class='col-md-6'>
+							<table class='table table-inverted'>
+							<h3>Turbidity</h3>
+							<tbody>"; 
+								foreach ($dataPoints3 as $data) {
+									echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
+								}
+		echo				"</tbody>
+							</table>
+						</div>
+						<div class='col-md-6'>
+							<div id='chartContainer3' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class='container' style='min-height: 105px;background-color: white;'>
+					<div class='row'>
+						<div class='col-md-6'>
+							<table class='table table-inverted'>
+							<h3>Conductivity</h3>
+							<tbody>";
+								foreach ($dataPoints4 as $data) {
+									echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
+								}
+		echo				"</tbody>
+							</table>
+						</div>
+						<div class='col-md-6'>
+							<div id='chartContainer4' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>";
+		echo "
+		<iframe 
+		   width='600' 
+		   height='350' 
+		   frameborder='0' 
+		   scrolling='no' 
+		   marginheight='0' 
+		   marginwidth='0' 
+		   src='http://maps.google.com/maps?f=q=colombo&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=".$positions_execute["Latitude"].",".$positions_execute["Longitude"].
+		   "(custom heading)&amp;output=embed'>
+		</iframe>";
+	}
+	else{
+		echo	"<div style='float:left;width:10%;background-color: grey;min-height:468px;'></div>
+				<div style='float:right;width:90%;'>
+					<div class='container' style='min-height: 105px;background-color: white;'>
+						<div class='row'>
+							<div class='col-md-6'>
+								<table class='table table-inverted'>
+								<h3>".$quality."</h3>
+								<tbody>"; 
+									foreach ($dataPoints as $data) {
+										echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
+									}						
+		echo					"</tbody>
+								</table>
+							</div>
+							<div class='col-md-6'>
+								<div id='chartContainer' style='min-height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
+							</div>
+						</div>
+					</div>
+				</div>";
+		echo "
+	<iframe 
+	   width='600' 
+	   height='350' 
+	   frameborder='0' 
+	   scrolling='no' 
+	   marginheight='0' 
+	   marginwidth='0' 
+	   src='http://maps.google.com/maps?f=q=colombo&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=".$positions_execute["Latitude"].",".$positions_execute["Longitude"].
+	   "(custom heading)&amp;output=embed'>
+	</iframe>";
 
-							// echo "<pre>"; 
-							foreach ($dataPoints1 as $data) {
-								echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
-							}						
-echo					"</tbody>
-				</table>
-			</div>
-			<div class='col-md-6'>
-				<div id='chartContainer1' style='min-height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
-			</div>
-		</div>
-	</div>
-	<br>
-	<div class='container' style='min-height: 105px;background-color: white;'>
-		<div class='row'>
-			<div class='col-md-6'>
-				<table class='table table-inverted'>
-					<h3>pH</h3>
-					<tbody>";
-
-							// echo "<pre>"; 
-							foreach ($dataPoints2 as $data) {
-								echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
-							}
-echo					"</tbody>
-				</table>
-			</div>
-			<div class='col-md-6'>
-				<div id='chartContainer2' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
-			</div>
-		</div>
-	</div>
-	<br>
-	<div class='container' style='min-height: 105px;background-color: white;'>
-		<div class='row'>
-			<div class='col-md-6'>
-				<table class='table table-inverted'>
-					<h3>Turbidity</h3>
-					<tbody>";
-
-							// echo "<pre>"; 
-							foreach ($dataPoints3 as $data) {
-								echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
-							}
-
-echo					"</tbody>
-				</table>
-			</div>
-			<div class='col-md-6'>
-				<div id='chartContainer3' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
-			</div>
-		</div>
-	</div>
-	<br>
-	<div class='container' style='min-height: 105px;background-color: white;''>
-		<div class='row'>
-			<div class='col-md-6'>
-				<table class='table table-inverted'>
-					<h3>Conductivity</h3>
-					<tbody>";
-
-							// echo "<pre>"; 
-							foreach ($dataPoints4 as $data) {
-								echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
-							}
-echo					"</tbody>
-				</table>
-			</div>
-			<div class='col-md-6'>
-				<div id='chartContainer4' style='height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
-			</div>
-		</div>
-	</div>
-	</div>
-	<div><br>";
-echo "
-<iframe 
-   width='600' 
-   height='350' 
-   frameborder='0' 
-   scrolling='no' 
-   marginheight='0' 
-   marginwidth='0' 
-   src='http://maps.google.com/maps?f=q=colombo&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=".$positions_execute["Latitude"].",".$positions_execute["Longitude"].
-   "(custom heading)&amp;output=embed'>
-</iframe>";
-echo "<div class='centered'>Centered</div>
-	</div>";
-}
-else{
-	echo	"<div style='float:left;width:10%;background-color: grey;min-height:468px;'></div>
-	<div style='float:right;width:90%;'>
-		<div class='container' style='min-height: 105px;background-color: white;'>
-		<div class='row'>
-			<div class='col-md-6'>
-				<table class='table table-inverted'>
-					<h3>".$quality."</h3>
-					<tbody>";
-
-							// echo "<pre>"; 
-							foreach ($dataPoints as $data) {
-								echo "<tr><td>".$date."</td><td>".$position."</td><td>".$data['label']."</td><td>".$data['y']."</td></tr>";
-							}						
-echo					"</tbody>
-				</table>
-			</div>
-			<div class='col-md-6'>
-				<div id='chartContainer' style='min-height: 100px; max-width: 100%; margin: 0px auto; float:right; width:75%;'></div>
-			</div>
-		</div>
-	</div>";
-	echo "
-<iframe 
-   width='600' 
-   height='350' 
-   frameborder='0' 
-   scrolling='no' 
-   marginheight='0' 
-   marginwidth='0' 
-   src='http://maps.google.com/maps?f=q=colombo&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=".$positions_execute["Latitude"].",".$positions_execute["Longitude"].
-   "(custom heading)&amp;output=embed'>
-</iframe>";
-echo "<div class='centered'>Centered</div>
-	</div>";
-
-}
+	}
 }
 else{
 	echo "<label align='center'>".$message1."<br>".$message2."</label>";
